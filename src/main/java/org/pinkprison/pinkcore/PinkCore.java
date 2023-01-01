20c711f6cc4c81f411e75534d644b71002ce1dc6
WildTooth <champenV@outlook.dk>
2023-01-01 23:00:56 +0100

    init commit

---
+++ src/main/java/org/pinkprison/pinkcore/PinkCore.java
@@ -0,0 +1,17 @@
+package org.pinkprison.pinkcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PinkCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
