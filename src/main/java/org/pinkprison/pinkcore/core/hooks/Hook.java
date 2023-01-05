853fbd844a988d0ebec2b5fa4a98a0a134b6ab44
WildTooth <champenV@outlook.dk>
2023-01-05 10:12:38 +0100

    Fixed an error with the class not being abstract

--- src/main/java/org/pinkprison/pinkcore/core/hooks/Hook.java
+++ src/main/java/org/pinkprison/pinkcore/core/hooks/Hook.java
@@ -4,11 +4,20 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 import org.pinkprison.pinkcore.core.interfaces.IHook;
 
-public class Hook implements IHook {
+/**
+ * Implementation of IHook for the dependencies
+ */
+public abstract class Hook implements IHook {
     private final String name;
     private final org.pinkprison.pinkcore.core.enums.Hook hook;
     private final boolean isEnabled;
 
+    /**
+     * Hook constructor
+     *
+     * @param paramString Hook name
+     * @param paramHook Hook enum
+     */
     public Hook(String paramString, org.pinkprison.pinkcore.core.enums.Hook paramHook){
         this.name = paramString;
         this.hook = paramHook;
@@ -17,16 +26,28 @@ public class Hook implements IHook {
         else this.isEnabled = Bukkit.getPluginManager().getPlugin(getName()) != null && Bukkit.getPluginManager().getPlugin(getName()).isEnabled();
     }
 
+    /**
+     *
+     * @return if the hook is enabled
+     */
     @Override
     public boolean isEnabled(){
         return isEnabled;
     }
 
+    /**
+     *
+     * @return the name of the hook
+     */
     @Override
     public @NotNull String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return the enum of the hook
+     */
     @Override
     public @NotNull org.pinkprison.pinkcore.core.enums.Hook getEnum() {
         return hook;
