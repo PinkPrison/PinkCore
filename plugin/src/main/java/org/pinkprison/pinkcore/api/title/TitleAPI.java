f66450c1f7026d7a110beefe87fa1632261ad810
WildTooth <champenv@outlook.dk>
2023-02-09 19:48:59 +0100

    Removed BukkitTitleAPI.java, InternalAPIMapping.java, InternalTitleAPI.java, MinecraftVersion.java and ReflectionTitleAPI.java. TitleAPI.java has everything necessary and is now Legacy versions only.

--- plugin/src/main/java/org/pinkprison/pinkcore/api/title/TitleAPI.java
+++ plugin/src/main/java/org/pinkprison/pinkcore/api/title/TitleAPI.java
@@ -1,8 +1,11 @@
 package org.pinkprison.pinkcore.api.title;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
-import java.util.Optional;
+import java.lang.reflect.Constructor;
+import java.util.Objects;
 
 /**
  * Title API
@@ -15,71 +18,115 @@ import java.util.Optional;
  * @since 1.0.0
  */
 public class TitleAPI {
-    static InternalTitleAPI internalApi = InternalAPIMapping.create();
 
     /** @deprecated */
     @Deprecated
     public static void sendTitle(Player player, int fadeIn, int stay, int fadeOut, String message) {
-        sendTitle(player, fadeIn, stay, fadeOut, message, null);
+        sendTitle(player, message, null, fadeIn, stay, fadeOut);
     }
 
     /** @deprecated */
     @Deprecated
     public static void sendSubtitle(Player player, int fadeIn, int stay, int fadeOut, String message) {
-        sendTitle(player, fadeIn, stay, fadeOut, null, message);
+        sendTitle(player, null, message, fadeIn, stay, fadeOut);
     }
 
     /** @deprecated */
     @Deprecated
     public static void sendFullTitle(Player player, int fadeIn, int stay, int fadeOut, String title, String subtitle) {
-        sendTitle(player, fadeIn, stay, fadeOut, title, subtitle);
-    }
-
-    /**
-     * Sends a title to the {@link Player}
-     *
-     * @param player The {@link Player} to send the title to
-     * @param fadeIn The time in ticks for the title to fade in
-     * @param stay The time in ticks for the title to stay on screen
-     * @param fadeOut The time in ticks for the title to fade out
-     * @param title The title text
-     * @param subtitle The subtitle text
-     */
-    public static void sendTitle(Player player, int fadeIn, int stay, int fadeOut, String title, String subtitle) {
         sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
     }
 
     /**
-     * Sends a title to the {@link Player}
+     * Sends a title to a {@link Player}.
      *
-     * @param player The {@link Player} to send the title to
-     * @param title The title text
-     * @param subtitle The subtitle text
-     * @param fadeIn The time in ticks for the title to fade in
-     * @param stay The time in ticks for the title to stay on screen
-     * @param fadeOut The time in ticks for the title to fade out
+     * @param player   The player to send the title to.
+     * @param title    The title to send.
+     * @param subtitle The subtitle to send.
+     * @param fadeIn   The time in ticks for the title to fade in.
+     * @param stay     The time in ticks for the title to stay on screen.
+     * @param fadeOut  The time in ticks for the title to fade out.
      */
     public static void sendTitle(Player player, String title, String subtitle, int fadeIn, int stay, int fadeOut) {
+        try {
+            Object e;
+            Object chatTitle;
+            Object chatSubtitle;
+            Constructor<?> subtitleConstructor;
+            Object titlePacket;
+            Object subtitlePacket;
 
+            if (title != null) {
+                title = ChatColor.translateAlternateColorCodes('&', title);
+                title = title.replaceAll("%player%", player.getDisplayName());
+                // Times packets
+                e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TIMES").get(null);
+                chatTitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke((Object) null, new Object[]{"{\"text\":\"" + title + "\"}"});
+                subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(getNMSClass("PacketPlayOutTitle").getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+                titlePacket = subtitleConstructor.newInstance(e, chatTitle, fadeIn, stay, fadeOut);
+                sendPacket(player, titlePacket);
 
-        InternalTitleAPI internalApi = getInternalApi().orElseThrow(() -> new RuntimeException("No internal API, Unsupported version?"));
+                e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TITLE").get(null);
+                chatTitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, new Object[]{"{\"text\":\"" + title + "\"}"});
+                subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"));
+                titlePacket = subtitleConstructor.newInstance(e, chatTitle);
+                sendPacket(player, titlePacket);
+            }
 
+            if (subtitle != null) {
+                subtitle = ChatColor.translateAlternateColorCodes('&', subtitle);
+                subtitle = subtitle.replaceAll("%player%", player.getDisplayName());
+                // Times packets
+                e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TIMES").get(null);
+                chatSubtitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + title + "\"}");
+                subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+                subtitlePacket = subtitleConstructor.newInstance(e, chatSubtitle, fadeIn, stay, fadeOut);
+                sendPacket(player, subtitlePacket);
 
-        internalApi.sendTitle(player, title, subtitle, fadeIn, stay, fadeOut);
+                e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("SUBTITLE").get(null);
+                chatSubtitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + subtitle + "\"}");
+                subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+                subtitlePacket = subtitleConstructor.newInstance(e, chatSubtitle, fadeIn, stay, fadeOut);
+                sendPacket(player, subtitlePacket);
+            }
+        } catch (Exception ignored) {}
     }
 
     /**
-     * Sends an empty title to the {@link Player}
+     * Sends an empty title to a {@link Player}.
      *
-     * @param player the {@link Player} to send the empty title to
+     * @param player The {@link Player} to send the empty title to.
      */
-    public static void clearTitle(Player player) {
-
-        InternalTitleAPI internalApi = getInternalApi().orElseThrow(() -> new RuntimeException("No internal API, Unsupported version?"));
-        internalApi.clearTitle(player);
+    public void clearTitle(Player player) {
+        sendTitle(player, "", "", 0, 0, 0);
     }
 
-    static Optional<InternalTitleAPI> getInternalApi() {
-        return Optional.ofNullable(internalApi);
+    /**
+     * Helper method to send a title packet to a {@link Player}.
+     *
+     * @param player The {@link Player} to send the packet to
+     * @param packet to send
+     */
+    private static void sendPacket(Player player, Object packet) {
+        try {
+            Object handle = player.getClass().getMethod("getHandle").invoke(player);
+            Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
+            playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
+        } catch (Exception e) {}
+    }
+
+    /**
+     * Gets a NMS class by name.
+     *
+     * @param name The name of the class.
+     * @return The class.
+     */
+    private static Class<?> getNMSClass(String name) {
+        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        try {
+            return Class.forName("net.minecraft.server." + version + "." + name);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
 }
