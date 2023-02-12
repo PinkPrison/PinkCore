f669bd11c9f965fe4427d3aead9b88a657548d4f
Peter537 <pean@outlook.dk>
2023-02-12 14:36:03 +0100

    Removed whitespace, changed variable names and more

--- plugin/src/main/java/org/pinkprison/pinkcore/api/utils/StringUtils.java
+++ plugin/src/main/java/org/pinkprison/pinkcore/api/utils/StringUtils.java
@@ -20,56 +20,77 @@ public class StringUtils {
      * if the list is empty, it will return a predetermined string.
      *
      * @param strings list of strings to format
-     * @param placeholder the placeholder to replace
-     *                    if the list is not empty
+     * @param placeholder the placeholder to replace if the list is not empty
      * @return formatted string
      */
     public static String formatList(List<String> strings, String placeholder){
-        if(strings.isEmpty()) return placeholder;
+        if (strings.isEmpty()) {
+            return placeholder;
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < strings.size(); i++) {
-            if (i != 0)
+            if (i != 0) {
                 sb.append(i == strings.size() - 1 ? " " + "og" + " " : ", ");
+            }
+
             sb.append(strings.get(i));
         }
+
         return sb.toString();
     }
 
     /**
-     *
      * Returns a boolean value from a string.
      *
-     * @param paramString the string to check
-     * @param paramStrings the strings to check against
+     * @param string the string to check
+     * @param strings the strings to check against
      *
      * @return if the string ends with any of the strings
      */
-    public static boolean endsWith(String paramString, String... paramStrings){
-        for(String s : paramStrings)
-            if(paramString.substring(paramString.length()-s.length()).equalsIgnoreCase(s))
+    public static boolean endsWith(String string, String... strings){
+        for (String s : strings) {
+            if (string.substring(string.length() - s.length()).equalsIgnoreCase(s)) {
                 return true;
+            }
+        }
+
         return false;
     }
 
     /**
      * Returns a boolean value from weather or not a string is a number.
      *
-     * @param s the string to check
+     * @param string the string to check
      * @return if the string is a number
      */
+    public static boolean isAlphabetical(String string) {
+        if (string == null) {
+            return false;
+        }
 
-    public static boolean isAlphabetical(String s) {
-        if (s == null) return false;
-        int len = s.length();
-        for (int i = 0; i < len; i++)
-            if (!Character.isLetter(s.charAt(i))) return false;
+        int len = string.length();
+        for (int i = 0; i < len; i++) {
+            if (!Character.isLetter(string.charAt(i))) {
+                return false;
+            }
+        }
+
         return true;
     }
 
-    public static String getCapitalized(String value){
+    /**
+     * Returns a capitalized string.
+     *
+     * @param string the string to capitalize
+     * @return the capitalized string
+     */
+    public static String getCapitalized(String string){
         List<String> list = new ArrayList<>();
-        for(String s : value.split(" "))
+        for (String s : string.split(" ")) {
             list.add(s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase());
+        }
+
         return String.join(" ", list);
     }
 }
