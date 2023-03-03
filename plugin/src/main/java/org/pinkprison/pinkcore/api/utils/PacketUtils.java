4c4e3e7ac4942f176a3376d122f87e1c55faa7a6
Peter537 <pean@outlook.dk>
2023-03-03 11:41:22 +0100

    Create PacketUtils.java

---
+++ plugin/src/main/java/org/pinkprison/pinkcore/api/utils/PacketUtils.java
@@ -0,0 +1,30 @@
+package org.pinkprison.pinkcore.api.utils;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Utility class for Packets.
+ *
+ * <p>
+ *     This class is part of the PinkCore project.
+ *     <a href="https://github.com/PinkPrison/PinkCore">PinkCore</a> is licensed under the MIT license.
+ * </p>
+ * @author PandaPeter
+ * @since 2.0.0
+ */
+public class PacketUtils {
+
+    /**
+     * Method to send a packet to a {@link Player}.
+     *
+     * @param player The {@link Player} to send the packet to
+     * @param packet to send
+     */
+    public static void sendPacket(Player player, Object packet) {
+        try {
+            Object handle = player.getClass().getMethod("getHandle").invoke(player);
+            Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
+            playerConnection.getClass().getMethod("sendPacket", NMSUtils.getNMSClass("Packet")).invoke(playerConnection, packet);
+        } catch (Exception ignored) { }
+    }
+}
